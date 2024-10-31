ba00b3ca4947fcd6596f8d137465eddaa56ec812
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2024-10-31 08:57:05 -0300

    layout

--- app/components/HeaderNav/index.js
+++ app/components/HeaderNav/index.js
@@ -1,16 +1,31 @@
 import Image from "next/image";
-import styles from "./headerNav.module.css"
+import styles from "./headerNav.module.css";
 import Link from "next/link";
+
 export default function HeaderNav({ usuario }) {
+
+  usuario = { nombre: "carlos" }; 
+
   return (
-    <header>
-        <ul>
-            <li><Link href={"/"}>Home</Link></li>
-            <li><Link href={"/contacto"}>Contacto</Link></li>
-        </ul>
-        <div>
-            
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <Image src={'/../../public/logo.png'} alt="Logo del sitio" width={100} height={50} />
+          
         </div>
+        <Link href="/">Home</Link>
+        <Link href="/contacto">Contacto</Link>
+      </nav>
+      {usuario ? (
+        <div className={styles.userMenu}>
+          <span className={styles.userName}>{usuario.nombre}</span>
+          <button className={styles.logoutButton}>Cerrar sesión</button>
+        </div>
+      ) : (
+        <Link href="/login" className={styles.loginButton}>
+          Iniciar sesión
+        </Link>
+      )}
     </header>
   );
 }
