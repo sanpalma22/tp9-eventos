85550f020167d439497e11673809ce7d75ed09d3
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2024-11-07 08:49:39 -0300

    terminado

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer style={{ background: '#f1f1f1', padding: '20px', textAlign: 'center' }}>
+    <footer>
       <p>© 2024 Ticketek. Todos los derechos reservados.</p>
       <a href="/contacto">Contacto</a>
     </footer>
